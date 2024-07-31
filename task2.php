52a3aad841550fe8e76e00859b22d65a6f063a5e
Suparn001 <suparngautam@gmail.com>
2024-07-31 13:25:17 +0530

    done

--- task2.php
+++ task2.php
@@ -1,72 +1,61 @@
 <?php
-class car 
+class Vehicle
 {
-    public $cbrand;
-    public $cmodel;
-    public $cmax_speed;
-    public $cheight;
-
-    public function __construct($cbrand,$cmodel,$cmax_speed,$cheight){
-     
-        $this->cheight  = $cheight;
-        $this->cbrand  = $cbrand;
-        $this->cmodel  = $cmodel;
-        $this->cmax_speed  = $cmax_speed;
-}
-public function getinfo(){
-    echo "<h1>Here is your Car details:</h1><br>";
-    echo "Car Brand:{$this->cbrand}"."<br>";
-    echo "Car Model:{$this->cmodel}"."<br>";
-    echo "Car Speed:{$this->cmax_speed} kmph"."<br>";
-    echo "Car Height:{$this->cheight} meters"."<br>";
-}
-}
-
-class truck 
-{
-    public $theight;
-    public $tbrand;
-    public $tmodel;
-    public $tspeed;
-
-    public function __construct($tbrand,$tmodel,$theight,$tspeed = 120){
-        $this->theight  = $theight;
-        $this->tbrand  = $tbrand;
-        $this->tmodel  = $tmodel;
-        $this->tspeed = $tspeed;
+    public $brand;
+    public $model;
+    public $max_speed;
+    public $height;
+    public $type;
+    public function __construct($type,$brand, $model,$max_speed,$height)
+    {
+        $this->type  = $type;
+        $this->height  = $height;
+        $this->brand  = $brand;
+        $this->model  = $model;
+        $this->max_speed  = $max_speed;
     }
-public function getinfo(){
-    echo "Here is your Truck details:<br>";
-    echo "Truck brand:{$this->tbrand}"."<br>";
-    echo "Truck Model:{$this->tmodel}"."<br>";
-    echo "Truck Speed:{$this->tspeed} kmph"."<br>";
-    echo "Truck Height:{$this->theight} meters"."<br>";
-}
+    public function getinfo()
+    {
+        echo "<h1>Here is your Vehicle details:</h1><br>";
+        echo "Vehicle Type:{$this->type}<br>";
+        echo "Vehicle Brand:{$this->brand}<br>";
+        echo "Vehicle Model:{$this->model}<br>";
+        echo "Vehicle Speed:{$this->max_speed} kmph<br>";
+        echo "Vehicle Height:{$this->height} meters<br>";
+    }
 }
 
-if($_SERVER["REQUEST_METHOD"]=="POST")
-{   
+
+class truck extends Vehicle
+{
+
+    public $max_speed;
+    public function __construct($type,$brand, $model,$height,$max_speed=120){
+     parent::__construct($type,$brand, $model,$max_speed,$height);
+   
+        $this->max_speed = $max_speed;
+    }
+    
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $type = $_POST['type'];
     $brand = $_POST['brand'];
     $model = $_POST['model'];
     $speed = $_POST['speed'];
     $height = $_POST['height'];
 
-if(strtolower($type)=== "car")
-{
-    $car = new car($brand,$model,$speed,$height);
-    $car->getinfo();
-}
+    if (strtolower($type) === "car") {
+        $car = new truck($type ,$brand, $model, $speed, $height);
+        $car->getinfo();
+    } 
+    elseif (strtolower($type) === "truck") {
 
-elseif(strtolower($type)==="truck"){
-   
-    $truck = new truck($brand,$model,$height);
-    $truck->getinfo();
+        $truck = new truck($type,$brand,$model,$height);
+        $truck->getinfo();
+    } 
+    else {
+        echo ("Your have entered wrong type of vechile..<br>");
+        echo ("You can enter only Car or Truck");
+    }
 }
-
-else{
-    echo ("Your have entered wrong type of vechile..<br>");
-    echo ("You can enter only Car or Truck");
-}
-}
-?>
