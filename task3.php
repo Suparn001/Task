a7f9ec67df9135cb4f1de50d0a8fbc8e95d01dd6
Suparn001 <suparngautam@gmail.com>
2024-07-31 15:49:18 +0530

    done

---
+++ task3.php
@@ -0,0 +1,39 @@
+<?php
+class Vehicle
+{
+    public $brand;
+    public $model;
+    public $max_speed;
+    
+    public $type;
+    public function __construct($type,$brand, $model,$max_speed=null)
+    {
+        $this->type  = $type;
+        $this->brand  = $brand;
+        $this->model  = $model;
+        if($this->type == "truck"){
+            $this->max_speed =120;
+         }
+else{
+    $this->max_speed  = $max_speed;
+}
+        
+        
+     
+    }
+    public function getinfo()
+    {
+        echo "<h1>Here is your Vehicle details:</h1><br>";
+        echo "Vehicle Type:{$this->type}<br>";
+        echo "Vehicle Brand:{$this->brand}<br>";
+        echo "Vehicle Model:{$this->model}<br>";
+        echo "Vehicle Speed:{$this->max_speed} kmph<br>";
+       
+    }
+}
+
+
+    $truck = new Vehicle("truck" ,"da", "civic");
+    $truck->getinfo();
+    
+?>
